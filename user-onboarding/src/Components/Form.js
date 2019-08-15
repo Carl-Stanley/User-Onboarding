b8de210a7bca1dfcd691b11ac6f4f247200707fe
Carl-Stanley <cds.beml@gmail.com>
2019-08-15 15:58:34 -0700

    Commit

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -2,13 +2,30 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 
 function LoginForm() {
-    return (
-      <Form>
-        <Field type="text" name="username" placeholder="Username" />
-        <Field type="password" name="password" placeholder="Password" />
-        <button>Submit!</button>
-      </Form>
-    );
+  return (
+    <Form>
+
+      <Field type="name" name="name" placeholder="Name" />  
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <Field type="checkbox" name="tos" placeholder="unchecked"/>  
+      <button>Submit!</button>
+    </Form>
+  );
+}
+
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+
+  handleSubmit(values) {
+    console.log(values);
+    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
   }
-  
-  export default LoginForm;
+})(LoginForm);
+
+export default FormikLoginForm;
