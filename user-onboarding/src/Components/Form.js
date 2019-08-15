f4304abdf1674c059aa442c4eca193407d80534e
Carl-Stanley <cds.beml@gmail.com>
2019-08-15 16:51:16 -0700

    Commit

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,30 +1,67 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-
-function LoginForm() {
+import * as Yup from "yup";
+import axios from "axios";
+function LoginForm({ values, errors, touched, isSubmitting }) {
   return (
     <Form>
-
-      <Field type="name" name="name" placeholder="Name" />  
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
-      <Field type="checkbox" name="tos" placeholder="unchecked"/>  
-      <button>Submit!</button>
+        <div>
+        {touched.Name && errors.Name && <p>{errors.Name}</p>}
+        <Field type="text" name="Name" placeholder="Your Name" />
+        </div>
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+      <label>
+        <Field type="checkbox" name="tos" checked={values.tos} />
+        Accept TOS
+      </label>
+      <button type="submit">Submit!</button>
     </Form>
   );
 }
 
 const FormikLoginForm = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, Name, tos }) {
     return {
+
       email: email || "",
-      password: password || ""
+      password: password || "",      
+      Name: Name || "",
+      tos : tos  || false
     };
   },
 
-  handleSubmit(values) {
-    console.log(values);
-    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be 6 characters or longer")
+      .required("Password is required"),
+     Name: Yup.string()
+       .required("Your Name is required"),
+    tos: Yup.boolean()
+       .required("You must accept the TOS!")  
+  }),
+
+  handleSubmit(values,{ resetForm, setErrors, setSubmitting }) {
+    axios
+        .post("https://reqres.in/api/users", values)
+        .then(res => {
+          console.log(res); // Data was created successfully and logs to console
+          resetForm();
+          setSubmitting(false);
+        })
+        .catch(err => {
+          console.log("Another Error? ",err); // There was an error creating the data and logs to console
+          setSubmitting(false);
+        });    
   }
 })(LoginForm);
 
