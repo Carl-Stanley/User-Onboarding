021007d38dcf22cd50ed8c562ccd0aaa40d32231
Carl-Stanley <cds.beml@gmail.com>
2019-08-15 15:48:10 -0700

    Commit

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,42 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
+import { withFormik, Form, Field } from "formik";
 
 function LoginForm() {
-  const [user, setUser] = useState({ username: "", password: "" });
-
-  const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(user.username);
-    console.log(user.password);
-  };
-
-  return (
-    <div className="loginForm">
-      {console.log(user)}
-      <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="text"
-            name="password"
-            onChange={event => handleChange(event)}
-          />
-        </label>
+    return (
+      <Form>
+        <Field type="text" name="username" placeholder="Username" />
+        <Field type="password" name="password" placeholder="Password" />
         <button>Submit!</button>
-      </form>
-    </div>
-  );
-}
-
-export default LoginForm;
+      </Form>
+    );
+  }
+  
+  export default LoginForm;
