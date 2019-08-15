fa6b85c638b314179266d6b35cadf8df0977d563
Carl-Stanley <cds.beml@gmail.com>
2019-08-15 15:42:33 -0700

    Commit

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Components/Form';
+import LoginForm from './Components/Form';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="App">     
+
+    <LoginForm />
+
     </div>
   );
 }
